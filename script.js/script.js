f0aba05a8823a0c29f182d7ed07c2786cafb211c
KristinaKripke <kristina.kripke@gmail.com>
2025-03-24 11:32:55 +0200

    Agency

--- script.js/script.js
+++ script.js/script.js
@@ -1,5 +1,9 @@
+const mobileNAv = document.querySelector('.header-main-nav');
+
 const mobileNavOpen = document.querySelector('.mobile-nav-open');
 
 mobileNavOpen.addEventListener('click', () => {
     mobileNavOpen.classList.toggle('active');
+    mobileNAv.classList.toggle('active');
 })
+
